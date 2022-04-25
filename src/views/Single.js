22b7c8dfbb39cfb538b45866ff1b1c23970313ea
Riku Immonen <rikuimm@metropolia.fi>
2022-04-25 19:49:39 +0300

    navigation

---
+++ src/views/Single.js
@@ -0,0 +1,20 @@
+import {mediaUrl} from '../utils/variables';
+import {useLocation} from 'react-router-dom';
+
+const Single = () => {
+  const location = useLocation();
+  console.log(location);
+  const file = location.state.file;
+  // TODO in the next task: single media from props.location.state
+
+  return (
+    <>
+      <h1>{file.title}</h1>
+      <img src={mediaUrl + file.filename} alt={file.title} />
+    </>
+  );
+};
+
+// TODO in the next task: add propType for location
+
+export default Single;
