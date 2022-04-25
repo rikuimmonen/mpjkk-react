3bd235a945fa250e45ab040b220da77c58ecaaf7
Riku Immonen <rikuimm@metropolia.fi>
2022-04-25 19:05:40 +0300

    http-c

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,30 +1,9 @@
 // import PropTypes from 'prop-types';
-import {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
 import MediaRow from './MediaRow';
+import {useMedia} from '../hooks/ApiHooks';
 
 const MediaTable = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  const getMedia = async () => {
-    try {
-      const mediaResponse = await fetch(baseUrl + 'media');
-      const media = await mediaResponse.json();
-      const allFiles = await Promise.all(
-          media.map(async (file) => {
-            const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
-            return await fileResponse.json();
-          }),
-      );
-      setMediaArray(allFiles);
-    } catch (err) {
-      alert(err.message);
-    }
-  };
-
-  useEffect(() => {
-    getMedia();
-  }, []);
-
+  const {mediaArray} = useMedia();
   return (
     <table>
       <tbody>
---
+++ src/hooks/ApiHooks.js
@@ -0,0 +1,42 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const fetchJson = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      const message = json.message;
+      throw new Error(message);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const getMedia = async () => {
+    try {
+      const media = await fetchJson(baseUrl + 'media');
+      const allFiles = await Promise.all(
+          media.map(async (file) => {
+            return await fetchJson(`${baseUrl}media/${file.file_id}`);
+          }),
+      );
+      setMediaArray(allFiles);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getMedia();
+  }, []);
+
+  return {mediaArray};
+};
+
+export {useMedia};
